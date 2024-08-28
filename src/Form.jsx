2009e01e321f1c8ae939a6d97a217148685188ee
Mahmood Moqadasi <mahmood1380.mm@gmail.com>
2024-08-28 13:56:45 +0330

    creating form section

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import styles from "./styles/Form.module.css";
+import comps from "styled-components";
+const Input = comps.input`
+  width:300px;
+  height:35px;
+  border: 1px rgb(110, 110, 110) solid;
+  border-radius:4px;
+  outline:none;
+  padding-left:5px;
+  &:focus{
+  border: 2px rgb(0, 180, 156) solid;
+  }
+  
+`;
+const Btn = comps.button`
+  width:616px;
+  height:40px;
+  border: none;
+  border-radius:4px;
+  background-color:rgb(0, 180, 156);
+  color:white;
+  cursor:pointer;
+  font-size:14px
+`;
 function Form() {
-  return <form></form>;
+  return (
+    <div className={styles.container}>
+      <form>
+        <Input type="text" placeholder="Name" />
+        <Input type="text" placeholder="Last Name" />
+        <Input type="text" placeholder="Email" />
+        <Input type="text" placeholder="Phone" />
+        <Btn>Add Contact</Btn>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
